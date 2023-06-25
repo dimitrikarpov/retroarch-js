a98a9103e123ba7f9efd1b20ccb3b9ff51a87aa2
dimitrikarpov <karpov.dimon@gmail.com>
2023-06-25 16:52:54 +0500

    update packages meta

--- examples/demo-retroarch-react-2/src/components/from-local/cores.ts
+++ examples/demo-retroarch-react-2/src/components/from-local/cores.ts
@@ -3,5 +3,3 @@ import { Mutable } from "../../utils"
 export const cores = ["fceumm", "genesis_plus_gx"] as const
 
 export type Core = Mutable<(typeof cores)[number]>
-
-type A = keyof (typeof cores)[number]
